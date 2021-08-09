a9cb9020cbab3998d044e9c7ad31a52c54699e03
Luke Blondin <lblondin747@gmail.com>
2021-08-09 14:35:12 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+        public bool isaPalindrome(string input)
+        {
+            input = input.ToLower();
+
+            char[] charArray = input.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            var reverse = new string(charArray);
+
+            return (input == reverse) ? true : false;
+            
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Rotator", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("Potato",false)]
+        public void isaPalindrome(string input, bool expected)
         {
+            var wordSmith = new WordSmith();
 
+            var actual = wordSmith.isaPalindrome(input);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
